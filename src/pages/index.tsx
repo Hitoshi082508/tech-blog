d5b05b6d4a41081bb5a34168edcd9554bb735cc0
Hitoshi <hitoshi082508@gmail.com>
2022-11-26 21:29:22 +0900

    chore: microCMSの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import Header from '@/components/Header'
+import Link from 'next/link'
+import { client } from '@/lib/client'
 
-export default function Home() {
+export default function Home({ blog }) {
   return (
     <div>
-      <Header />
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/${blog.id}`}>
+              <div>{blog.title}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: 'blogs' })
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  }
 }
