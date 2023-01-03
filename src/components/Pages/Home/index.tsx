e2c75299406fd6b103b53cc38d0dd495cef1b5a6
Hitoshi <hitoshi082508@gmail.com>
2023-01-03 17:00:11 +0900

    feature: 職業詳細のUIを作成

--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -1,8 +1,9 @@
 import About from '@/components/Templates/About'
-import Category from '@/components/Templates/Category'
+import JobList from '@/components/Templates/JobList'
 import Member from '@/components/Templates/Member'
 import Top from '@/components/Templates/Top'
 import Voice from '@/components/Templates/Voice'
+import { theme } from '@/styles/theme'
 import { Contents } from '@/types/contents'
 
 type HomePageProps = {
@@ -14,7 +15,12 @@ const HomePage: React.FC<HomePageProps> = ({ data }) => {
     <>
       <Top data={data} />
       <About />
-      <Category />
+      <JobList
+        title="Category"
+        subTitle="なりたい像から自分に合った職を見つけよう"
+        backgroundColor={theme.color.main}
+        fontColor={theme.color.white}
+      />
       <Voice />
       <Member />
     </>
